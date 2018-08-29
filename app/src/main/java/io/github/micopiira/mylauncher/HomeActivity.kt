5e136900976f5ae5ada87f27d97631e2f0c9fe66
Mico Piira <mico.piira@gmail.com>
2018-08-29 20:51:35 +0300

    Open app preferences on long click

--- app/src/main/java/io/github/micopiira/mylauncher/HomeActivity.kt
+++ app/src/main/java/io/github/micopiira/mylauncher/HomeActivity.kt
@@ -1,9 +1,12 @@
 package io.github.micopiira.mylauncher
 
 import android.app.Activity
+import android.content.Intent
 import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.provider.Settings
 import android.widget.SearchView
 
 import kotlinx.android.synthetic.main.activity_apps_list.*
@@ -26,6 +29,10 @@ class HomeActivity : Activity() {
         apps_list.numColumns = Integer.parseInt(sharedPreferences.getString(if (isPortrait) "grid_columns_portrait" else "grid_columns_landscape", "4"))
         apps_list.adapter = appsAdapter
         apps_list.setOnItemClickListener { _, _, position, _ -> startActivity(appsAdapter.filteredApps[position].intent) }
+        apps_list.setOnItemLongClickListener { _, _, position, _ ->
+            startActivity(Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).setData(Uri.fromParts("package", appsAdapter.filteredApps[position].name, null)))
+            return@setOnItemLongClickListener true
+        }
         searchview.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(p0: String?): Boolean {
                 return false
